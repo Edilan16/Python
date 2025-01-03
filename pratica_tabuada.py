674d2ed303a80d9d653151931759c451a0bb9cd4
Edilan16 <edilan.gomes.54@gmail.com>
2025-01-03 00:21:56 -0300

    commit 2025
    primeiro commit 2025

---
+++ pratica_tabuada.py
@@ -0,0 +1,46 @@
+# print('De qual numero voê quer saber a tabuada ? ')
+# numero = input('Digite um numero: ')
+# valor = int(numero)
+# multiplica = 1,2,3,4,5,6,7,8,9,10
+# total= 0
+# print(multiplica)
+# if valor >0:
+#     for indice in multiplica:
+#         total = valor*indice
+#         print(valor,'*' ,indice ,':', total)
+ 
+
+
+# calculadora simples
+# try:
+#     valor1 = input('Digite 1º valor: ')
+#     valor1_int = int(valor1)
+#     valor2 = input('Digite 2º valor: ')
+#     valor2_int = int(valor2)
+#     operacao = input('Digite a operação matematica [+] [-] [/] [*]: ')
+#     resultado = 0
+
+#     if valor1.isdigit() and valor2.isdigit():
+#         if  operacao == '+':
+#             resultado = valor1_int+ valor2_int
+#             print(f'O resultado de {valor1_int} + {valor2_int} é :',resultado)
+
+#         if operacao =='-':
+#             resultado = valor1_int - valor2_int
+#             print(f'O resultado de {valor1_int} - {valor2_int} é :',resultado)
+
+#         if operacao =='*':
+#             resultado = valor1_int * valor2_int
+#             print(f'O resultado de {valor1_int} * {valor2_int} é :',resultado)
+
+#         if operacao =='/':
+#             resultado = valor1_int / valor2_int
+#             print(f'O resultado de {valor1_int} / {valor2_int} é :',resultado)
+#     # 
+# except(ValueError):
+#      print('Seu numero não é válido')
+#      print('Digite apenas numeros')
+     
+
+# NUMEROS PRIMOS
+
